6fa2b348b3c6c5f8b4bb10a82466613fb230880c
EddyGHendrickx <matthijs.hendrickx@gmail.com>
2020-05-29 11:32:22 +0200

    merging

---
+++ script.js
@@ -0,0 +1,10 @@
+let img1 = document.getElementById('img1');
+let img2 = document.getElementById('img2');
+console.log('allo');
+img2.style.display = "hidden";
+
+img1.addEventListener("click", function () {
+
+
+
+}
